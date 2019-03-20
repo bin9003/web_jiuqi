534245d94f49fd94ef6a86812eb43973f89a8bf1
porform <thesuperego@126.com>
2019-03-21 02:11:56 +0800

    首页轮播更改

--- src/components/carousel/throughly.vue
+++ src/components/carousel/throughly.vue
@@ -9,10 +9,8 @@
             data-slotamount="15"
             data-masterspeed="300"
             data-link="/download/index.jhtml"
-            v-for="list in banner"
-            :key="list.key"
           >
-            <img :src="list.src" alt>
+            <img :src="bannerNow.src">
           </li>
         </ul>
       </div>
@@ -20,11 +18,36 @@
   </div>
 </template>
 <script lang="ts">
-import { Component, Vue, Prop } from 'vue-property-decorator'
+import { Component, Vue, Prop, Watch } from 'vue-property-decorator'
 
 @Component
 export default class Throughly extends Vue {
   @Prop(Array) readonly banner !: Object
+  data () {
+    return {
+      bannerNow: {},
+      count: '0'
+    }
+  }
+  @Watch('banner')
+  onChildChanged (val: string, oldVal: string) {
+    // console.log(this.banner)
+    this.$set(this.$data, 'bannerNow', this.banner[0]) // 设置第一个banner 图片
+    this.bannerCarousel()
+  }
+  bannerCarousel () {
+    let This = this
+    let index = this.$data.count
+    setInterval(function () {
+      index++
+      if (index >= This.banner.length) {
+        index = 0
+      }
+      This.$set(This.$data, 'count', index)
+      This.$set(This.$data, 'bannerNow', This.banner[index])
+      console.log(index, This.$data.count)
+    }, 3000)
+  }
 }
 </script>
 <style lang="scss">
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -20,9 +20,9 @@ import axios from 'axios'
 export default class Home extends Vue {
   mounted () {
     let _this = this
+    // 可以使用 vue-resource 加载跨域数据 也可以使用 webpack 配置进行跨域请求
     axios.get('/data/appData.json').then(function (response) {
-      _this.$set(_this.$data, 'home', response.data)
-      // console.log(response, _this.$data)
+      _this.$set(_this.$data, 'home', response.data.home)
     }).catch(function (error) {
       console.log(error)
     })
